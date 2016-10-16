e57fb16e2d66bbecfa1e4c99bf2caf1cac901197
Chungjaehong <esizjh@naver.com>
2016-10-16 21:09:45 +0900

    commit

---
+++ js/dataPick.js
@@ -0,0 +1,121 @@
+(function(){
+
+	testPro = function(){
+		myProgress = new AXProgress();
+		myProgress.setConfig({
+			theme: "AXCircleProgress",
+			totalCount: 100,
+			width: 400,
+			top: 100,
+			title: "계정정보 동기화 중입니다...",
+			duration: 50 // 프로세스바의 애니메이션 속도 값 입니다.
+		});
+
+		mask.open();
+		myProgress.start(function(){
+			if(this.isEnd){
+				myProgress.close();
+				mask.close();
+				myGrid.setList(grpArData, null, "reload");
+			}else{
+				// 무언가 처리를 해줍니다.	대부분 비동기 AJAX 통신 처리 구문을 수행합니다.
+				myProgress.update(); // 프로그레스의 다음 카운트를 시작합니다.
+			}
+		});
+	}
+	
+
+
+	campArData = new Array();
+	grpArData = new Array();
+
+	grpUrl = "";
+	cmpUrl = "";
+	gCampId ="";
+	gCampName="";
+	grpUrlMoveFlag =0;
+
+	//948185
+	$("#btnUser").click(function(){
+			var url = "https://manage.searchad.naver.com/customers/"+customerId+"/campaigns";
+			cmpUrl = url; 
+			urlMove(url,"CAMP");
+			
+			//init variable
+			campArData = new Array();
+			grpArData = new Array();
+			grpUrlMoveFlag = 0;
+
+			testPro();
+			
+	});
+
+	dataPickCmp = function(){//camp data pick
+			var arTable=$("#ifrView").contents().find('table tr[id]');
+			var arCamp = $("#ifrView").contents().find('table tr td a.link');
+			campArData = new Array();
+
+			if(arTable.length >0){
+					clearInterval(intervalCmpFunction);
+					
+					for(var i=0; i < arCamp.length ; i++){
+							var campInfo = new Object();
+							campInfo.name = arCamp[i].innerText;
+							campInfo.id = arTable[i].id.replace("wgt-","");
+							campInfo.flag = arCamp[i].parentElement.previousElementSibling.outerText;
+							
+							gCampId = campInfo.id;
+
+							campArData.push(campInfo);
+					}//캠페인 아이디 네임 가져오기
+
+					for(var i=0 ; i<campArData.length;i++){
+							
+							if(campArData.length == grpUrlMoveFlag){
+									break;
+							}
+							
+							var url = "https://manage.searchad.naver.com/customers/"+customerId+"/campaigns/"+campArData[grpUrlMoveFlag].id+"?recordSize=1001";
+							grpUrl = url;
+
+							if(campArData.length > grpUrlMoveFlag){
+									arCamp[grpUrlMoveFlag].click();
+									urlMove(url,'GROUP');
+							}
+					}
+			}
+	}
+
+
+	dataPickGrp = function(){//group data pick
+			var grpList = $("#ifrView").contents().find('li.active.open-list ul li a');
+
+			if(grpList.length > 0){
+					clearInterval(intervalGrpFunction);
+					
+					for(var i =0; i < grpList.length ; i++){
+								var grpInfo = new Object();
+								grpInfo.id = grpList[i].attributes[1].value;
+								grpInfo.name = grpList[i].innerText;
+								grpInfo.campId = gCampId;
+								grpInfo.CampName = campArData[grpUrlMoveFlag].name;
+
+								grpArData.push(grpInfo);
+					}
+
+					var url = "https://manage.searchad.naver.com/customers/"+customerId+"/campaigns?recordSize=1001";
+					cmpUrl = url;
+					if(campArData.length < grpUrlMoveFlag){
+										
+					}else{
+							urlMove(url,"CAMP");
+							grpUrlMoveFlag++;
+					}
+			}
+	}
+
+	urlMove = function(url,flag){
+		$("#ifrView").attr("src",url);
+	}//Url Move Func
+
+}());
---
+++ main.js
@@ -0,0 +1,89 @@
+const electron = require('electron')
+// Module to control application life.
+const app = electron.app
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+const {Menu} = require('electron')
+
+const template = [
+  {
+    label: '로그인',
+    submenu: [
+      {
+        label: '네이버',
+		    enabled : true,
+		    click (item, menuClick) {
+          mainWindow.loadURL(`file:${__dirname}/main.html`)
+        }
+      },
+	  {
+        label: '다음',
+		    //enabled : false
+        enabled : true,
+		    click (item, menuClick) {
+          mainWindow.loadURL(`file:${__dirname}/test.html`)
+        }
+      }
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [
+      {
+        label: 'Reload',
+        accelerator: 'CmdOrCtrl+R',
+        click (item, focusedWindow) {
+          if (focusedWindow) focusedWindow.reload()
+        }
+      },
+      {
+        label: 'Toggle Developer Tools',
+        accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+        click (item, focusedWindow) {
+          if (focusedWindow) focusedWindow.webContents.toggleDevTools()
+        }
+      }
+    ]
+  }
+]
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 800, height: 600})
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+
+}
+
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
